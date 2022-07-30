7c6104d28a0466001ac328a2c4f3669a175d72b3
georgemarwanqana@gmail.com <georgemarwanqana@gmail.com>
2022-07-30 12:23:37 +0200

    added new google analytics code

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7ea5419d0951fc50"; }
+/******/ 		__webpack_require__.h = function() { return "be0b9d23888a414e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
