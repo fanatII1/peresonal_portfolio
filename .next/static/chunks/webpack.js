1ab2755eceb44523e5afd6f25fe3b4e0d097f9bd
georgemarwanqana@gmail.com <georgemarwanqana@gmail.com>
2022-07-30 13:02:03 +0200

    new google analytics code

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "be0b9d23888a414e"; }
+/******/ 		__webpack_require__.h = function() { return "6e865e14f4af27c9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.df87bc4a00465bcd.hot-update.js
+++ .next/static/webpack/webpack.df87bc4a00465bcd.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "be0b9d23888a414e"; }
+/******/ 	__webpack_require__.h = function() { return "6e865e14f4af27c9"; }
 /******/ }();
 /******/ 
 /******/ }
