fa96fa7a1ce981c4cb56894deaf8555eb7d86d37
georgemarwanqana@gmail.com <georgemarwanqana@gmail.com>
2022-07-30 10:12:26 +0200

    added google analytics code

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ecc22e2e8dede4bf"; }
+/******/ 		__webpack_require__.h = function() { return "8ef58d1341728d51"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.8daf5491ffc5bd1d.hot-update.js
+++ .next/static/webpack/webpack.8daf5491ffc5bd1d.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "e3c18259101a88a4"; }
+/******/ 	__webpack_require__.h = function() { return "8e28dfe8ce3ace54"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/make namespace object */
